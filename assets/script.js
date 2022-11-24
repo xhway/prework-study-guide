b320f743fe8ea18953c3e320b763b2a861104780
Xhway <dhway20@gmail.com>
2022-11-23 22:10:59 -0500

    Added JavaScript

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+var topic = "HTML";
+
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+
+    console.log(topics[x]);
+}
+
+
+/*
+if (topic === 'HTML') {
+
+    console.log("Lets study HTML!");
+
+}   else if (topic === 'CSS') {
+
+    console.log("Let's study CSS!");
+
+}   else if (topic === 'Git') {
+
+    console.log("Lets study Git!");
+
+}   else if (topic === 'JavaScript') {
+
+    console.log("Lets study JavaScript!");
+
+}   else {
+
+    console.log('Please try again!');
+
+}
+*/
+
+
+
+
