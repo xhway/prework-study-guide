2f32556b457a2738ce109a52233c2dee3ade2847
Xhway <dhway20@gmail.com>
2022-11-25 16:25:04 -0500

    Added more JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,46 @@
-var topic = "HTML";
-
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+var randomTopic = topics[Math.floor(Math.random()* topics.length)]
+
+function listTopics() {
 for (var x = 0; x < topics.length; x++) {
 
     console.log(topics[x]);
+ } 
 }
 
+function selectTopic() {
 
-/*
-if (topic === 'HTML') {
+
+    if (randomTopic === 'HTML') {
 
     console.log("Lets study HTML!");
 
-}   else if (topic === 'CSS') {
+    }   else if (randomTopic === 'CSS') {
 
     console.log("Let's study CSS!");
 
-}   else if (topic === 'Git') {
+    }   else if (randomTopic === 'Git') {
 
     console.log("Lets study Git!");
 
-}   else if (topic === 'JavaScript') {
+    }   else if (randomTopic === 'JavaScript') {
 
     console.log("Lets study JavaScript!");
 
-}   else {
+    }   else {
 
     console.log('Please try again!');
 
+  }
 }
-*/
 
+console.log('Here are the topics we learned through Prework:');
 
+listTopics()
+
+console.log('Which topic should we study first?');
+
+selectTopic()
 
 
